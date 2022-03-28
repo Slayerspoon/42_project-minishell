2f889f93406988a8c18929dda0532832dc94a8bc
Kacper Pucylo <kpucylo@c2s16d99.42wolfsburg.de>
2022-03-28 17:39:15 +0200

    bruh im dumb

--- execute.c
+++ execute.c
@@ -6,7 +6,7 @@
 /*   By: kpucylo <kpucylo@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/21 15:31:14 by kpucylo           #+#    #+#             */
-/*   Updated: 2022/03/28 15:29:43 by kpucylo          ###   ########.fr       */
+/*   Updated: 2022/03/28 17:38:27 by kpucylo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -101,6 +101,8 @@ void	be_patient(int amount)
 }
 
 //execute when piping (not sure how to indicate its needed yet)
+//I JUST REALIZED THIS IS VERY DUMB IM PASSING THE SAME COMMAND TO BOTH PROCESSES
+//NEED TO FIGURE OUT HOW TO DO THIS PROPERLY
 void	exec_piped(char *cmd, char **flags, char **envp)
 {
 	int		pipe_fd[2];
